dbcfbc9bd90de1a83160168f4317c9912ee187fb
Yotam <yotam@archimededsfi.com>
2022-03-23 15:01:08 -0700

    Add Vault contract and tests

--- contracts/OUSD.sol
+++ contracts/OUSD.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OUSD is ERC20 {
     constructor() ERC20("oUSD", "oUSD") { // do we want to talk about what needs to be here?
-        _mint(msg.sender, 1000000);
+        _mint(msg.sender, 1000);
         // Find a way to send money to customers 
     }
 }
